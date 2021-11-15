9f819f2fde328e09a408ea720fdc499737c3b27b
Ignacio Amatt <ignacio_amatt@hotmail.com>
2021-11-15 16:11:42 -0300

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -50,6 +50,7 @@ module.exports = function (eleventyConfig) {
       includes: "/_src/layouts",
       data: "/_src/data",
       output: "_dist",
+      pathPrefix: "/Prep-Course-Stage/"
     },
   };
 };
